db7beed2483a8d1b6c8ae6ff10c2f4eea9db45f9
Reo <pokk123123a@gmail.com>
2026-03-08 22:28:23 +0900

    1-7-10926 3rd commit

--- 1-7-10926.c
+++ 1-7-10926.c
@@ -5,8 +5,11 @@ int main(void) {
     char ID[50];
     scanf("%s", ID);
 
-    if (strcmp("joonas", ID) == 0) {
-        printf("%s??!\n", ID);
-    }
+    // if (strcmp("joonas", ID) == 0) {
+    //     printf("%s?\?!\n", ID);
+    //     //트라이그래프를 나타내는 것이 아닌 기호 그자체를 나타내고 싶을떄는 \를 입력해서 다음것을 특수문자로 인식하지 않도록 해야한다.
+    // }
+
+    printf("%s?\?!\n", ID);
     return 0;
 }
