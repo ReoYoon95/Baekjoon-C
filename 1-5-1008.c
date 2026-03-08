a79fa891e90c05700330729b550be0d798159e07
Reo <pokk123123a@gmail.com>
2026-03-08 22:00:18 +0900

    1-5-1008 4th commit

--- 1-5-1008.c
+++ 1-5-1008.c
@@ -3,6 +3,12 @@
 int main(void) {
     int A, B;
     scanf("%d %d", &A, &B);
-    printf("%0.12lf\n", (double)A / B);
+    if(B == 0)  {
+        printf("Division by zero is not allowed.\n");  
+    }
+    else {
+        printf("%0.12lf\n", (double)A / B);
+    }
+    
     return 0;
 }
