e2ea211c8f5e7a6e287e33c1c6110f40891721d4
Reo <pokk123123a@gmail.com>
2026-03-09 13:57:23 +0900

    1-10-2588 change for BJ.style

--- 1-10-2588.c
+++ 1-10-2588.c
@@ -2,19 +2,13 @@
 
 int main(void) {
     int A,B;
-    printf("Insert 1st Number : \t");
     scanf("%d", &A);
-    printf("Insert 2nd Number : \t");
     scanf("%d", &B);
 
-    printf("%7d\n%7d\n", A, B);
-    //A&B 첫번째 둘째줄에 넣는 작업.
-    printf("---------------------------\n");
-    printf("%7d\n", A * (B % 10));
-    printf("%6d\n", A * (B / 10 % 10));
-    printf("%5d\n", A * (B / 100));
-    printf("---------------------------\n");
-    printf("%7d\n", A * B);
+    printf("%d\n", A * (B % 10));
+    printf("%d\n", A * (B / 10 % 10));
+    printf("%d\n", A * (B / 100));
+    printf("%d\n", A * B);
 
     return 0;
 
